3c6e7d1dd65fa4a76934e01ba1354e1cb8cc722f
jecaro <jeancharles.quillet@gmail.com>
2025-06-11 10:07:58 +0200

    Add dev shell for supported versions of ghc

--- flake.nix
+++ flake.nix
@@ -33,19 +33,53 @@
 
       checks = self.packages;
 
-      devShell = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in
-        pkgs.haskellPackages.shellFor {
-          packages = p: [ self.packages.${system}.systranything ];
-          withHoogle = true;
-          buildInputs = [
-            pkgs.haskellPackages.cabal-install
-            pkgs.haskellPackages.ghcid
-            pkgs.haskellPackages.haskell-ci
-            pkgs.haskellPackages.haskell-language-server
-            pkgs.zenity
-          ];
-        });
+      devShells = forAllSystems
+        (system:
+          let pkgs = nixpkgsFor.${system};
+          in
+          {
+            # Default shell for development
+            default = pkgs.haskellPackages.shellFor {
+              packages = p: [ self.packages.${system}.systranything ];
+              withHoogle = true;
+              buildInputs = [
+                pkgs.haskellPackages.cabal-install
+                pkgs.haskellPackages.ghcid
+                pkgs.haskellPackages.haskell-ci
+                pkgs.haskellPackages.haskell-language-server
+                pkgs.zenity
+              ];
+            };
+          } //
+          # Additional shells for the compiler versions we support
+          pkgs.lib.attrsets.genAttrs [ "ghc948" "ghc964" "ghc982" ] (
+            ghc: pkgs.haskell.packages.${ghc}.shellFor {
+              # No package in scope. One need to call `cabal update` and start
+              # for here.
+              packages = p: [ ];
+              buildInputs = [
+                pkgs.haskellPackages.cabal-install
+
+                # dependencies of haskell-gi and the ayatana bindings
+                pkgs.gobject-introspection
+                pkgs.gtk3
+                pkgs.lerc
+                pkgs.libayatana-appindicator
+                pkgs.libdatrie
+                pkgs.libepoxy
+                pkgs.libselinux
+                pkgs.libsepol
+                pkgs.libsysprof-capture
+                pkgs.libthai
+                pkgs.libxkbcommon
+                pkgs.pcre2
+                pkgs.pkg-config
+                pkgs.util-linux
+                pkgs.xorg.libXdmcp
+                pkgs.xorg.libXtst
+              ];
+            }
+          )
+        );
     };
 }
